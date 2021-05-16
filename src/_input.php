d1ac384355ba0c5d0b76502e8ebb7789ca4c7051
Hill <hill@kimo.com>
2021-05-17 06:20:43 +0800

    update input

--- src/_input.php
+++ src/_input.php
@@ -11,15 +11,23 @@ class Input
     {
         $c = \PMVC\plug('controller');
         $req = $c->getRequest();
+        $uri = \PMVC\get(\PMVC\passByRef(\PMVC\plug('url')));
+        unset($uri['this']);
+        $uri['path'] = \PMVC\plug('url')->getPath();
         $result = [
             'request'=> \PMVC\get($req),
             'action'=> $c->getAppAction(),
-            'app'=> $c->getApp(),
-            'app-real' => $c[_REAL_APP],
+            'app'=> [
+              'pass' => $c->getApp(),
+              'real' => $c[_REAL_APP],
+              'help' => 'You could check alias info with folders->search->app->alias',
+            ],
             'template' => [
               'dir' => $c[_TEMPLATE_DIR],
               'engine' => $c[_VIEW_ENGINE],
             ],
+            'uri' => $uri,
+            'router' => $c[_ROUTER],
             'folders' => [
               'apps' => $c[_RUN_APPS],
               'site' => $c->getAppsParent(),
