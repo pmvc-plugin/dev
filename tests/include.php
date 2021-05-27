23aa9179b60a360d9d9b27826fe2e314d430bbaf
Hill <hill@kimo.com>
2021-05-27 18:19:14 +0000

    format

--- tests/include.php
+++ tests/include.php
@@ -3,6 +3,9 @@
 $path = __DIR__ . '/../vendor/autoload.php';
 include $path;
 
-\PMVC\Load::plug(['unit' => null, 'debug'=>false], [__DIR__ . '/../../']);
+\PMVC\Load::plug(
+  ['unit' => null, 'debug'=>false],
+  [__DIR__ . '/../../']
+);
 
 \PMVC\l(__DIR__ . '/resources/FakeDebugOutput');
